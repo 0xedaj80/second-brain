06d28fb586fc84bae6ad6d702d3a6bd94a35ab6e
0xedaj80 <0xedaj8080@gmail.com>
2024-12-04 20:32:52 +0530

    added error Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,14 +9,25 @@ function App() {
     <RecoilRoot>
     <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Landing></Landing>}></Route> 
       <Route path="/Signup" element={<Signup></Signup>}></Route> 
       <Route path="/Signin" element={<Signin></Signin>}></Route> 
       <Route path="/Dashboard" element={<Dashboard></Dashboard>}></Route>
       <Route path="/Landing" element={<Landing></Landing>}></Route>  
+      <Route path="*" element={<PageNotFound></PageNotFound>}></Route>  
       </Routes>    
     </BrowserRouter>
 </RecoilRoot>
    )
+}
+
+
+function PageNotFound(){
+    return (
+      <div className="flex justify-center items-center text-4xl">
+         page not found
+      </div>
+    )
 }
 
 export default App
